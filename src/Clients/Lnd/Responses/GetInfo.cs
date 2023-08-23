98ac73d76036f1f703f4694e86992d454ffda92d
Gary Woodfine <garywoodfine@duck.com>
2023-08-23 22:26:32 +0100

    Added some more unit tests

--- src/Clients/Lnd/Responses/GetInfo.cs
+++ src/Clients/Lnd/Responses/GetInfo.cs
@@ -2,7 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace BoltPay.Lnd.Responses;
 
-internal class GetInfo
+public class GetInfo
 {
   [JsonPropertyName("alias")]
   public string Alias { get; set; }
--- tests/PayBolt.Tests/ClientTests/LndTests/LndClientTests.cs
+++ tests/PayBolt.Tests/ClientTests/LndTests/LndClientTests.cs
@@ -1,6 +1,12 @@
 using System.ComponentModel;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using BoltPay.Exceptions;
 using BoltPay.Lnd;
+using BoltPay.Lnd.Responses;
+using PayBolt.DependencyInjection;
+using Shouldly;
 
 namespace BoltPay.Tests.ClientTests.LndTests;
 
@@ -9,6 +15,31 @@ public class LndClientTests
     [Fact, Description("Should throw an exception if the URI is invalid")]
     public void ShouldThrowAnExceptionIfTheUriIsInvalid()
     {
-       Assert.Throws<PayBoltException>(() => LndClient.Create("bad_uri"));
+       Should.Throw<PayBoltException>(() => LndClient.Create("bad_uri"));
     }
+    
+    [Fact, Description("Should throw an exception if the URI is null")]
+    public void ShouldThrowAnExceptionIfTheUriIsNull()
+    {
+        Should.Throw<PayBoltException>(() => LndClient.Create(null!));
+    }
+    
+   [Fact, Description("Should Connect if Node Alias is not empty")] 
+   public async Task ShouldConnectIfNodeAliasIsNotEmpty()
+   {
+       var mockHttpMessageHandlerHandler = new MockHttpMessageHandler(
+           (JsonSerializer.Serialize(new GetInfo { Alias = "test" }), HttpStatusCode.OK));
+       
+       var client = new HttpClient(mockHttpMessageHandlerHandler);
+         var lndClient = LndClient.Create("http://localhost:8080", httpClient: client);
+
+        var result = await lndClient.Connect();
+        
+        result.ShouldSatisfyAllConditions(
+            _ => mockHttpMessageHandlerHandler.Requests.Count.ShouldBe(1),
+            _ => mockHttpMessageHandlerHandler.Requests[0].RequestUri.ShouldBeEquivalentTo(new Uri("http://localhost:8080/v1/getinfo")),
+            _ => result.Result.ShouldBe(Result.Ok)
+            );
+   }
+    
 }
